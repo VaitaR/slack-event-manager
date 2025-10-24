a4f25e2749693b3688af0665567dfa2ac037cff3
VaitaR <andrey.shivalin@phystech.edu>
2025-10-24 14:15:28 +0300

    Refactor import of SlackClient in ingest_messages.py

--- src/use_cases/ingest_messages.py
+++ src/use_cases/ingest_messages.py
@@ -15,7 +15,7 @@ from typing import TYPE_CHECKING, Any, Final
 
 import pytz
 
-from src.clients.slack_wrapped import SlackClient as WrappedSlackClient
+from src.clients.slack_wrapped import SlackClient
 from src.adapters.slack_state_store import GetConnectionCallable, SlackStateStore
 from src.config.logging_config import get_logger
 from src.config.settings import Settings
